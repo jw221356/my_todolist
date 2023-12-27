4912d0628ef8dd89f32a84694e3a293d8e94ddd1
jeong won <jw221356@naver.com>
2023-12-27 17:06:41 +0900

    모델클래스 추가 후 리스트 표시

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:my_todolist/create_screen.dart';
+import 'package:my_todolist/todo.dart';
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  final todos = [
+    Todo(
+      title: 'title 1',
+      dateTime: 12321,
+    ),
+    Todo(
+      title: 'title 2',
+      dateTime: 12321123,
+    ),
+    Todo(
+      title: 'title 2',
+      dateTime: 12321123,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,23 +32,18 @@ class ListScreen extends StatelessWidget {
         title: const Text('Todo 리스트'),
       ),
       body: ListView(
-        children: const[
-          ListTile(
-            title: Text('title 1'),
-            subtitle: Text('subtitle 1'),
-          ),
-          ListTile(
-            title: Text('title 2'),
-            subtitle: Text('subtitle 2'),
-          ),
-        ],
+        children: todos
+            .map((todo) => ListTile(
+                  title: Text(todo.title),
+                  subtitle: Text('${todo.dateTime}'),
+                ))
+            .toList(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) =>
-              const CreateScreen()),
+            context,
+            MaterialPageRoute(builder: (context) => const CreateScreen()),
           );
         },
         child: const Icon(Icons.add),
